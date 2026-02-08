935d8eaccf46752d864e836f3ada97b0249f4096
Ra-Jo1 <jonatra1@gmail.com>
2026-02-08 21:28:09 +0100

    Create DHT11_LCD1602_Photoresistor_CSV_Data_PYTHON.py
    There is 2 codes 
    First Arduino code in C++ for the electronic
    Second Python to grab the data 
    
    // ----------------HERE THE SECOND CODE IN PYTHON :-----------------

---
+++ DHT11_LCD1602_Photoresistor_CSV_Data_PYTHON.py
@@ -0,0 +1,33 @@
+# There is 2 codes 
+# First Arduino code in C++ for the electronic
+# Second Python to grab the data 
+
+# ----------------HERE THE SECOND CODE IN PYTHON :-----------------
+
+import csv
+import os
+import serial
+from datetime import datetime
+
+com = 'COM7'
+baud = 9600
+
+ser = serial.Serial(com,baud)
+
+csv_file = "Light_Humidity_Temperature_data.csv"
+
+file_exists = os.path.isfile(csv_file)
+
+with open(csv_file, mode = 'a',newline = '',encoding = 'utf-8') as file:
+  writer = csv.writer(file)
+
+  if not file_exists:
+    writer.writerow(['date','light','humidity %','temperature °c'])
+
+  while True:
+    line = ser.readline().decode('utf-8').strip()
+
+    if  line:
+      now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+      writer.writerow([now, line])
+      print(now, line)
